40ff817b684f0feb880667d385f520de0515579e
Razvan Paduraru <razvan.paduraru@stud.acs.upb.ro>
2019-11-14 15:18:38 +0200

    Initial commit

---
+++ src/main/java/com/orange/proiecttest/ProiectTestApplication.java
@@ -0,0 +1,13 @@
+package com.orange.proiecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orange/proiecttest/ProiectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.proiecttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
